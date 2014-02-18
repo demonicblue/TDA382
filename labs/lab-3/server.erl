47fb6c60e7319a856ac69d8ae78e389e1e089e1d
Simon Fransson <frollix@razzan.net>
2014-02-18 21:25:24 +0100

    Fixed how clients are added at server, also added disconnect functionality

--- labs/lab-3/server.erl
+++ labs/lab-3/server.erl
@@ -4,13 +4,20 @@
 -include_lib("./defs.hrl").
 
 loop(St, {connect, _ClientId, _Nick}) ->
-	NewList = lists:append(St#server_st.clients, _ClientId),
+	NewList = lists:append(St#server_st.clients, [_ClientId]),
 	X = St#server_st{clients = NewList},
+	io:format("contents: ~p~n", [NewList]),
+	{ok, X};
+
+loop(St, {disconnect, _ClientId, _Nick}) ->
+	io:format("Username: ~p~n", [_ClientId]),
+	DelList = lists:delete(_ClientId, St#server_st.clients),
+	X = St#server_st{clients = DelList},
+	io:format("contents of deleted list: ~p~n", [X]),
 	{ok, X};
 
 loop(St, _Msg) -> 
     {ok, St}. 
-
 
 initial_state(_Server) ->
     #server_st{name = "shire", clients = []}.
