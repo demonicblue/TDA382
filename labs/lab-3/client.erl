23acb82e66b8f450f8d4da03f5532bf27dfbae00
Simon Fransson <frollix@razzan.net>
2014-02-18 21:25:02 +0100

    Added client functionality for disconnect.

--- labs/lab-3/client.erl
+++ labs/lab-3/client.erl
@@ -24,8 +24,19 @@ loop(St, {connect, _Server}) ->
 %%%%%%%%%%%%%%%
 %%%% Disconnect
 %%%%%%%%%%%%%%%
-loop(St, disconnect) ->
-     {ok, St} ;
+loop(St, disconnect) -> % Ha server som en variabel i Client-state?
+    %case whereis(list_to_atom(_Server)) of
+    %    undefined ->
+    %        Return = {error, "Cannot disconnect from server"};
+     %   _ ->
+    Result = catch_fatal(fun() -> genserver:request(list_to_atom("shire"), {disconnect, self(), St#cl_st.nick}) end),
+    Return = case Result of
+        ok  ->  trace(["Client got ok"]),
+                {ok, St};
+        error -> trace(["Client got error"]),
+                {error, "Fatal during disconnect"}
+    end,
+    Return; 
 
 %%%%%%%%%%%%%%
 %%% Join
