a054f18e55990ecd9f8db599516c1bda4617f3b7
Simon Fransson <frollix@razzan.net>
2014-02-19 13:07:39 +0100

    Fixed disconnect function.

--- labs/lab-3/client.erl
+++ labs/lab-3/client.erl
@@ -14,7 +14,8 @@ loop(St, {connect, _Server}) ->
             Result = catch_fatal(fun() -> genserver:request(list_to_atom(_Server), {connect, self(), St#cl_st.nick}) end),
             Return = case Result of
                  ok     ->  trace(["Client:got ok"]),
-                            {ok, St};
+                            NewState = St#cl_st{server = _Server},
+                            {ok, NewState};
                  error  ->  trace(["Client:got error"]),
                             {error, "Fatal during connect"}
             end
@@ -24,17 +25,20 @@ loop(St, {connect, _Server}) ->
 %%%%%%%%%%%%%%%
 %%%% Disconnect
 %%%%%%%%%%%%%%%
-loop(St, disconnect) -> % Ha server som en variabel i Client-state?
-    %case whereis(list_to_atom(_Server)) of
-    %    undefined ->
-    %        Return = {error, "Cannot disconnect from server"};
-     %   _ ->
-    Result = catch_fatal(fun() -> genserver:request(list_to_atom("shire"), {disconnect, self(), St#cl_st.nick}) end),
-    Return = case Result of
-        ok  ->  trace(["Client got ok"]),
-                {ok, St};
-        error -> trace(["Client got error"]),
-                {error, "Fatal during disconnect"}
+loop(St, disconnect) -> 
+    case St#cl_st.server of
+    "" ->
+        Return = {error, St};
+        
+    _ ->
+        Result = catch_fatal(fun() -> genserver:request(list_to_atom(St#cl_st.server), {disconnect, self(), St#cl_st.nick}) end),
+        Return = case Result of
+            ok  ->  trace(["Client got ok"]),
+                    NewState = St#cl_st{server = ""},
+                    {ok, NewState};
+            error -> trace(["Client got error"]),
+                    {error, St}
+        end
     end,
     Return; 
 
@@ -64,7 +68,7 @@ loop(St, whoiam) ->
     {St#cl_st.nick, St} ;
 
 %%%%%%%%%%
-%%% Nick
+%%% Nick""
 %%%%%%%%%%
 loop(St,{nick,_Nick}) ->
     X = St#cl_st{nick = _Nick},
@@ -106,4 +110,4 @@ trace(Args) ->
 
 
 initial_state(Nick, GUIName) ->
-    #cl_st { gui = GUIName, nick = Nick }.
+    #cl_st { gui = GUIName, nick = Nick, server = "" }.
