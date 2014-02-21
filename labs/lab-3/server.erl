af2dfd8b530566f795f115d1dc901130a63a9397
Henrik Hugo <hhugo@student.chalmers.se>
2014-02-21 12:35:52 +0100

    Implemented the join command

--- labs/lab-3/server.erl
+++ labs/lab-3/server.erl
@@ -19,8 +19,14 @@ loop(St, {disconnect, _Nick}) ->
 	{ok, X};
 
 loop(St, {join, _Nick, _Channel}) ->
-	NewList = list:append(St#server_st.nick_to_channel, {_Nick, _Channel}),
-	X = St#server_st{nick_to_channel = NewList},
+	case dict:is_key(_Channel, St#server_st.nick_to_channel) of
+		false ->
+			NewDict = dict:append(_Channel, [_Nick], St#server_st.nick_to_channel);
+		true ->
+			NewDict = dict:update(_Channel, fun(_List) -> lists:append(_List, [_Nick]) end, St#server_st.nick_to_channel)
+	end,
+	X = St#server_st{nick_to_channel = NewDict},
+	%io:format(dict:fetch(_Channel, X#server_st.nick_to_channel)),
 	{ok, X};
 
 loop(St, {msg_from_client, _Nick, _Channel, _Msg}) ->
@@ -39,4 +45,4 @@ distribute([], _Channel, _Nick, _Msg) ->
 	ok.
 
 initial_state(_Server) ->
-    #server_st{name = "shire", clients = dict:new(), nick_to_channel = []}.
+    #server_st{name = "shire", clients = dict:new(), nick_to_channel = dict:new()}.
